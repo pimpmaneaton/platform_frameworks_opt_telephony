235af4c70017b92a579cdc2ebffd62eac24aa40a
Roman Birg <roman@cyngn.com>
2016-05-05 16:21:11 -0700

    Properly detect when MSIM has 1 SIM
    We cannot use sSlotIdxToSubId to determine when to turn the SIM dialog
    off, because when subscription information gets reloaded (on data change
    for instance), we may add one, then disable SMS prompt, then add the
    other, which is incorrect. Query the total number of SIMs from the info
    updater.
    
    Ticket: CYNGNOS-2185, CYNGNOS-2570
    
    Change-Id: Iab1225c83b508e055d8a8de55c1e21e2aa153ca5

--- src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
+++ src/java/com/android/internal/telephony/SubscriptionInfoUpdater.java
@@ -117,6 +117,7 @@ public class SubscriptionInfoUpdater extends Handler {
     private int mCurrentlyActiveUserId;
     private CarrierServiceBindHelper mCarrierServiceBindHelper;
     private boolean mIsShutdown;
+    private int mCurrentSimCount;
 
     public SubscriptionInfoUpdater(
             Looper looper, Context context, Phone[] phone, CommandsInterface[] ci) {
@@ -681,6 +682,8 @@ public class SubscriptionInfoUpdater extends Handler {
             }
         }
 
+        mCurrentSimCount = insertedSimCount;
+
         if (!mIsShutdown && insertedSimCount == 1) {
             SubscriptionInfo sir = subInfos.get(0);
             int subId = sir.getSubscriptionId();
@@ -811,6 +814,10 @@ public class SubscriptionInfoUpdater extends Handler {
             }
         }
         return -1;
+    }
+
+    protected int getInsertedSimCount() {
+        return mCurrentSimCount;
     }
 
     private boolean isNewSim(String iccId, String[] oldIccId) {
